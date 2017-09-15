1ffef17a1cb13671da8caa8eeaf39ee37a71d386
Pete <shivers9000@gmail.com>
2017-09-15 08:45:26 +0000

    Q game comming along

---
+++ index.php
@@ -0,0 +1,20 @@
+<html><body>
+<ul>
+    <li>
+        <a href="777/">777</a>
+    </li>
+    <li>
+        <a href="Homework 1/">Homework 1</a>
+    </li>
+    <li>
+        <a href="Homework 2/">Homework 2</a>
+    </li>
+    <li>
+        <a href="Lab1/">Lab1</a>
+    </li>
+    <li>
+        <a href="LabActivity1/">LabActivity1</a>
+    </li>
+</ul>
+</body>
+</html>
